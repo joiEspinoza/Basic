82e1fc5b46fa1658161ed0b791a28b17d7fde63a
jose E <jo.espinoza.91@gmail.com>
2020-12-25 15:00:33 -0400

    adicion boton hi -devJoseE-

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ title.innerText = "Basic";
 
 const nameDev = document.querySelector( "h4" );
 nameDev.innerText = "Dev : Jose E";
+
+const buttonHi = document.querySelector( "button" );
+buttonHi.addEventListener( "click", ()=>{ alert( "hi" ) } );
